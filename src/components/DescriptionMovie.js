3d5ad0324ba5c970621b094170aa10b0b721b83e
paugal <50529290+paugal@users.noreply.github.com>
2024-01-13 20:24:35 +0100

    DescriptionMovie part 1

--- src/components/DescriptionMovie.js
+++ src/components/DescriptionMovie.js
@@ -1,38 +1,18 @@
 import '../style/App.css';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import '../style/DescriptionMovie.css'
 import React from 'react';
 
 
 function DescriptionMovie(){
 
     return(
-        <div>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                    sx={{ height: 140 }}
-                    image="https://image.tmdb.org/t/p/w1280/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg"
-                    title="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
-            </Card>
+        
+        <div className='fullInfoMovieBox'>
+            <div className='backdrop'>
+                <img src='https://image.tmdb.org/t/p/w1280/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg' alt='dune'></img>
+            </div>
         </div>
+        
     );
 }
 
