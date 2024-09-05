c394ecd1a6097036e3c09e3662c32b57bfc07be9
paugal <pau.galan22@gmail.com>
2024-09-06 00:14:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router basename="/whattowatch">
+      <Router>
         <TopVar />
         <div className="centeredDiv">
           <MovieProvider>
