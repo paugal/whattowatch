642a3846ad8410c416304863bcb4308234147cc9
paugal <pau.galan22@gmail.com>
2024-09-05 18:38:59 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import './style/App.css';
-import SelectThree from './components/SelectThree';
-import Recomendations from './components/Recomendations'
-import React, { useState } from 'react';
-import TopVar from './components/TopBar';
-import DescriptionMovie from './components/DescriptionMovie';
-import { MovieProvider } from './components/contextos/MovieContext';
-import PopularMovies from './components/PopularMovies';
-import SearchMovies from './components/SearchMovies';
-import {
-  BrowserRouter as Router, Routes,
-  Route
-} from "react-router-dom";
-
+import "./style/App.css";
+import SelectThree from "./components/SelectThree";
+import Recomendations from "./components/Recomendations";
+import React, { useState } from "react";
+import TopVar from "./components/TopBar";
+import DescriptionMovie from "./components/DescriptionMovie";
+import { MovieProvider } from "./components/contextos/MovieContext";
+import PopularMovies from "./components/PopularMovies";
+import SearchMovies from "./components/SearchMovies";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   const [selectedMovies, setSelectedMovies] = useState([]);
 
   const updateSelectedMovies = (movies) => {
@@ -22,22 +17,27 @@ function App() {
   };
 
   return (
-      <div className="App">
-        <TopVar/>
-        <div className="centeredDiv">
-          <Router>
-              <MovieProvider>
-                <Routes>
-                    <Route path="/" element={ <SelectThree updateSelectedMovies={updateSelectedMovies}/>} />
-                    <Route path="/film/:id"
-                        element={ <DescriptionMovie/>} />
-                    <Route path="/popular" element={ <PopularMovies/>} />
-                    <Route path="/search/:name" element={ <SearchMovies/>} />
-                </Routes>
-              </MovieProvider>
-          </Router>
-        </div>
+    <div className="App">
+      <TopVar />
+      <div className="centeredDiv">
+        <Router>
+          <MovieProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <SelectThree updateSelectedMovies={updateSelectedMovies} />
+                }
+              />
+              <Route path="/film/:id" element={<DescriptionMovie />} />
+              <Route path="/popular" element={<PopularMovies />} />
+              <Route path="/search/:name" element={<SearchMovies />} />
+            </Routes>
+          </MovieProvider>
+        </Router>
       </div>
+    </div>
   );
 }
+
 export default App;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,22 +1,22 @@
-import '../style/App.css';
-import '../style/TopBar.css';
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faTimes } from '@fortawesome/free-solid-svg-icons';
+import "../style/App.css";
+import "../style/TopBar.css";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function TopVar() {
   const [isSearching, setIsSearching] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = () => {
-    if(searchQuery != ''){
-        window.location.href = `/search/${searchQuery}`;
+    if (searchQuery != "") {
+      window.location.href = `/search/${searchQuery}`;
     }
-    
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
     }
   };
@@ -26,44 +26,44 @@ function TopVar() {
   };
 
   return (
-    <div className='TopBarContainer'>
-      <div className='TopBarCenter'>
-        <a id='Logo' href={`/`}>
+    <div className="TopBarContainer">
+      <div className="TopBarCenter">
+        <Link id="Logo" to="/">
           WhatToWatch
-        </a>
-        <div className='topBarButtons'>
-          <a href={`/popular`}>POPULAR</a>
-          <a href={`/watchlist`}>WATCHLIST</a>
-          <a href={`/perfil`}>PERFIL</a>
-          <div className='SearchBarTop'>
+        </Link>
+        <div className="topBarButtons">
+          <Link to="/popular">POPULAR</Link>
+          <Link to="/watchlist">WATCHLIST</Link>
+          <Link href="/perfil">PERFIL</Link>
+          <div className="SearchBarTop">
             {!isSearching ? (
-              <div id='startBuscar' onClick={toggleSearchBar}>
+              <div id="startBuscar" onClick={toggleSearchBar}>
                 BUSCAR
               </div>
             ) : (
-                <div className='SearchBoxInput'>
-                    <FontAwesomeIcon
-                    onClick={toggleSearchBar}
-                    id='cruzBuscador'
-                    icon={faTimes}
-                    size='lg'
-                    style={{ color: '#f0f5ff' }}
-                    />
-                    <div className="buscador">
-                    <input
-                        id='barraBuscador'
-                        autoFocus
-                        onKeyDown={handleKeyPress}
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                    <FontAwesomeIcon
-                        className='lupaIcon'
-                        onClick={handleSearch}
-                        icon={faMagnifyingGlass}
-                        style={{ color: '#faebd7' }}
-                    />
-                    </div>
+              <div className="SearchBoxInput">
+                <FontAwesomeIcon
+                  onClick={toggleSearchBar}
+                  id="cruzBuscador"
+                  icon={faTimes}
+                  size="lg"
+                  style={{ color: "#f0f5ff" }}
+                />
+                <div className="buscador">
+                  <input
+                    id="barraBuscador"
+                    autoFocus
+                    onKeyDown={handleKeyPress}
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  />
+                  <FontAwesomeIcon
+                    className="lupaIcon"
+                    onClick={handleSearch}
+                    icon={faMagnifyingGlass}
+                    style={{ color: "#faebd7" }}
+                  />
+                </div>
               </div>
             )}
           </div>
